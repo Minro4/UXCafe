54204a3645adcdc2e9a115cc9a269715f0bc01e6
William O'Sullivan-Dargis <william19.od@gmail.com>
2019-03-19 21:50:10 -0400

    xv

--- src/src/Cafe.java
+++ src/src/Cafe.java
@@ -1,30 +1,55 @@
 package src;
 
 public class Cafe {
-	
+
 	private final float ProportionSucreMax = 0.1f;
-	
+	private final float ProportionLaitMax = 0.5f;
+	private final float ProportionCremeMax = 0.1f;
+	private final int QuantiteParPortion = 15;
+
+	private Taille taille;
 	private int prtnSucre;
 	private int prtnLait;
 	private int prtnCreme;
-	
+
 	public int getPrtnSucre() {
 		return prtnSucre;
 	}
-	public void setPrtnSucre(int prtnSucre) {
-		
-		this.prtnSucre = prtnSucre;
+
+	public boolean setPrtnSucre(int prtnSucre) {
+		int portionMax = (int) (taille.getCapacite() * ProportionSucreMax);
+		if (portionMax <= prtnSucre) {
+			this.prtnSucre = prtnSucre;
+			return true;
+		}
+		return false;
 	}
+
 	public int getPrtnLait() {
 		return prtnLait;
 	}
-	public void setPrtnLait(int prtnLait) {
-		this.prtnLait = prtnLait;
+
+	public boolean setPrtnLait(int prtnLait) {
+		int quantiteLait = prtnLait * QuantiteParPortion;
+		int maxQuantiteLait = (int) (ProportionLaitMax * taille.getCapacite());
+		if (quantiteLait <= maxQuantiteLait) {
+			this.prtnLait = prtnLait;
+			return true;
+		}
+		return false;	
 	}
+
 	public int getPrtnCreme() {
 		return prtnCreme;
 	}
-	public void setPrtnCreme(int prtnCreme) {
-		this.prtnCreme = prtnCreme;
+
+	public boolean setPrtnCreme(int prtnCreme) {
+		int quantiteCreme = prtnCreme * QuantiteParPortion;
+		int maxQuantiteCreme = (int) (ProportionCremeMax * taille.getCapacite());
+		if (quantiteCreme <= maxQuantiteCreme) {
+			this.prtnCreme = prtnCreme;
+			return true;
+		}
+		return false;	
 	}
 }
