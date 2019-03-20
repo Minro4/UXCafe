14305c1c5f5700aec460bd90b007db0adc3a58f2
Jimmy <jimmyhoude97@gmail.com>
2019-03-19 21:56:08 -0500

    Ajout Debut Frame

---
+++ src/src/VueCafe.java
@@ -0,0 +1,14 @@
+package src;
+
+import javax.swing.JFrame;
+
+public class VueCafe extends JFrame {
+	public VueCafe() {
+		// ‐‐‐‐‐‐‐‐‐‐‐‐‐‐ Fenetre JFrame ‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+		setTitle("Cafe-Expresse");
+		setSize(640,480);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+	}
+}
