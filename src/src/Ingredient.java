926cf4f354be943a92f2b3463da7d6ab3dab282f
William O'Sullivan-Dargis <william19.od@gmail.com>
2019-03-29 21:59:24 -0400

    h

--- src/src/Ingredient.java
+++ src/src/Ingredient.java
@@ -7,6 +7,13 @@ public abstract class Ingredient {
 	private Image image; //jsp si cest ca que vs voulez
 	private String nom;
 
+	
+	public Ingredient(Image image, String nom) {
+		super();
+		this.image = image;
+		this.nom = nom;
+	}
+
 	public String getNom() {
 		return nom;
 	}
