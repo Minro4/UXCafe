dbf91684a8bebb3edcc8f5edd2ffa1b5aff853f8
Jimmy <jimmyhoude97@gmail.com>
2019-04-08 12:56:53 -0500

    Ajout Boisson
    Ajoute la classe abstraite boisson

--- src/src/Cafe.java
+++ src/src/Cafe.java
@@ -240,45 +240,7 @@ public class Cafe {
 
 		return rapport;
 	}
-	/*
-	 * public ArrayList<String[]> getRapport() { ArrayList<Map.Entry<String,
-	 * Double>> rapport = new ArrayList<Map.Entry<String, Double>>();
-	 * rapport.add(createEntry(getQuantiteCafe() + " ml caf�:", taille.getPrix()));
-	 * 
-	 * double prixTotal = 0;
-	 * 
-	 * { // On ajoute les jets int nbrPrtnJet = nbrTotPortionsJet(); for
-	 * (Map.Entry<Jet, Integer> entry : jets.entrySet()) { double ratio = (double)
-	 * entry.getValue() / nbrPrtnJet; String text = entry.getKey().rapport(ratio,
-	 * taille.getCapacite()); double prix = Jet.getPrix(ratio,
-	 * taille.getCapacite()); prixTotal += prix; rapport.add(createEntry(text,
-	 * prix)); } }
-	 * 
-	 * {// On ajoute le lait, creme et sucre String text =
-	 * lait.getKey().rapport(lait.getValue()); double prix =
-	 * lait.getKey().getPrix(lait.getValue()); prixTotal += prix;
-	 * rapport.add(createEntry(text, prix));
-	 * 
-	 * text = creme.getKey().rapport(creme.getValue()); prix =
-	 * creme.getKey().getPrix(creme.getValue()); prixTotal += prix;
-	 * rapport.add(createEntry(text, prix));
-	 * 
-	 * text = sucre.getKey().rapport(sucre.getValue()); prix =
-	 * sucre.getKey().getPrix(); prixTotal += prix; rapport.add(createEntry(text,
-	 * prix)); }
-	 * 
-	 * Collections.sort(rapport, new Comparator<Map.Entry<String, Double>>() {
-	 * public int compare(Map.Entry<String, Double> e1, Map.Entry<String, Double>
-	 * e2) { if (e1.getValue() > e2.getValue()) return 1; if (e1.getValue() ==
-	 * e2.getValue()) return 0; else return -1; } });
-	 * 
-	 * rapport.add(createEntry("Total", prixTotal));
-	 * 
-	 * return rapport; }
-	 * 
-	 * private Map.Entry<String, Double> createEntry(String key, Double value) {
-	 * return new AbstractMap.SimpleEntry<String, Double>(key, value); }
-	 */
+	
 
 	private int nbrTotPortionsJet() {
 		int sum = 0;
